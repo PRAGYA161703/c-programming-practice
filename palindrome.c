cc378a8b36bfa6993007ac240dfa23161aea55ac
PRAGYA161703 <25bcs123@iiitdwd.ac.in>
2026-01-27 18:45:24 +0530

    Create palindrome.c
    By this code we can recognize any palindrome number. code is same as reverse the digits and after that compare the original and reversed number if it is true then it is a palindrome.

---
+++ palindrome.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+    int n, original, rev=0, digit;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+
+    original = n;
+    while(n != 0) {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+    if(original == rev) {
+        printf(" The number is a palindrome.\n");
+    } else {
+        printf(" The number is not a palindrome.\n");
+    }
+    return 0;
+}
