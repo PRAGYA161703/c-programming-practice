b5e66742e075b5ed72f30cc658b8a57fafa278ca
PRAGYA161703 <25bcs123@iiitdwd.ac.in>
2026-07-09 12:40:54 +0530

    Uses strlen to count and return the exact number of characters present in a string.
    It stops counting right before the null terminator (\0), meaning it measures the actual length of your text, not the size of the memory container holding it.

---
+++ length_string.c
@@ -0,0 +1,9 @@
+// Write a C program to find the length of the string "Programming" using the strlen() function. 
+#include <stdio.h> 
+#include <string.h> 
+ 
+int main() { 
+    char name[] = "Programming"; 
+    printf("Length = %ld\n", strlen(name));   
+    return 0; 
+}
